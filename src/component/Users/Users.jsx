33e157ad61e359bb1d9c338d2371d055b2d07887
shahinexy <shahinexy@gmail.com>
2024-03-21 12:49:31 +0600

    cards

--- src/component/Users/Users.jsx
+++ src/component/Users/Users.jsx
@@ -1,15 +1,32 @@
 import { useLoaderData } from "react-router-dom";
-
+import './Users.css'
 
 const Users = () => {
 
     const user = useLoaderData()
-
+    console.log(user);
     return (
         <div>
             <h2>Our users</h2>
             <h3>Users Length: {user.length}</h3>
-            <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum a ducimus commodi tenetur! Pariatur, itaque.</div>
+            <div className="card_container">
+                {
+                    user.map(user => (
+                        <div key={user.id} className="card">
+                            <h2>name</h2>
+                            <hr />
+                            <ul>
+                                <li>
+                                    Phone: {user.phone}
+                                </li>
+                                <li>
+                                    Email: {user.email}
+                                </li>
+                            </ul>
+                        </div>
+                    ))
+                }
+            </div>
         </div>
     );
 };
